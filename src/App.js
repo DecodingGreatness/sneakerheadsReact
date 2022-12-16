c2ce9134d79d9ef8d83d477822d6c4c06a410a1a
Edward Ho <pythoned77@gmail.com>
2022-12-16 23:52:41 +0800

    matched leaf route error resolve but editing component to element in route for app.js

--- src/App.js
+++ src/App.js
@@ -7,10 +7,8 @@ function App() {
     <Router>
       <Navbar />
       <Routes>
-        <Route path="/" exact component={Home} />
+        <Route path="/" element={<Home />} exact />
       </Routes>
-
-      <h1>Shoes</h1>
     </Router>
   );
 }
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,9 +1,11 @@
 import React from "react";
 
 const Home = () => {
-  <div>
-    <h2>Shoes</h2>
-  </div>;
+  return (
+    <div>
+      <h2>Shoes</h2>
+    </div>
+  );
 };
 
 export default Home;
