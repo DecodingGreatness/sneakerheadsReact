e3366f447f7ad54223eb1a7bcf465a609d7bcc37
Edward Ho <pythoned77@gmail.com>
2022-12-16 23:28:36 +0800

    added home route to app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <Router>
       <Navbar />
+      <Routes>
+        <Route path="/" exact component={Home} />
+      </Routes>
 
-      <h1>Hello</h1>
+      <h1>Shoes</h1>
     </Router>
   );
 }
