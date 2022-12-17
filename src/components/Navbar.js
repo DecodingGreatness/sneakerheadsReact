d6b0ee75455350ac9f7000a53e33deb17d0cb13e
Edward Ho <pythoned77@gmail.com>
2022-12-17 15:03:36 +0800

    added mui material package and added AppBar to navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,20 +1,23 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "./navbar.css";
+import AppBar from "@mui/material/AppBar";
 
 const Navbar = () => {
   return (
-    <div className="navbar">
-      <li>
-        <Link to="/Home">Home</Link>
-      </li>
-      <li>
-        <Link to="/Men">Men</Link>
-      </li>
-      <li>
-        <Link to="/Female">Female</Link>
-      </li>
-    </div>
+    <AppBar position="static">
+      <div className="navbar">
+        <li>
+          <Link to="/Home">Home</Link>
+        </li>
+        <li>
+          <Link to="/Men">Men</Link>
+        </li>
+        <li>
+          <Link to="/Female">Female</Link>
+        </li>
+      </div>
+    </AppBar>
   );
 };
 
