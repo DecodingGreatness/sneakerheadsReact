8ce7387fc3df5f8cba0053767dc52112594a123e
Edward Ho <pythoned77@gmail.com>
2022-12-18 12:54:26 +0800

    updated navbar buttons

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,12 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import styles from "./navbar.module.css";
 import AppBar from "@mui/material/AppBar";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
 import Box from "@mui/material/Box";
-import Menu from "@mui/material/Menu";
-import MenuItem from "@mui/material/MenuItem";
+import IconButton from "@mui/material/IconButton";
+import MenuIcon from "@mui/icons-material/Menu";
+import Button from "@mui/material/Button";
 
 const pages = ["Men", "Female", "Kids", "Favorites"];
 
@@ -14,15 +14,23 @@ const Navbar = () => {
   return (
     <AppBar position="static" style={{ backgroundColor: "black" }}>
       <Toolbar disableGutters>
-        {/* <Box>
-          <Menu>
-            {pages.map((page) => (
-              <MenuItem key={page}>
-                <Typography>{page}</Typography>
-              </MenuItem>
-            ))}
-          </Menu>
-        </Box> */}
+        <Box>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          >
+            <MenuIcon />
+          </IconButton>
+
+          {pages.map((page) => (
+            <Button key={page}>
+              <Typography variant="h6">{page}</Typography>
+            </Button>
+          ))}
+        </Box>
         <Typography>
           <Link to="/Home">Home</Link>
           <Link to="/Men">Men</Link>
